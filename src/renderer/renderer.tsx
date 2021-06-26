8cfcff235f9a62b3244d7ac418ffb43a60776a16
Dyllon <dyllon.g@outlook.com>
2021-06-26 14:08:10 +0800

    fix config

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -4,13 +4,14 @@
 // Import the styles here to process them with webpack
 import '_public/style.css';
 
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
 
 ReactDOM.render(
-  <div className="app">
-    <h4>Welcome to React, Electron and Typescript</h4>
-    <p>Hello</p>
-  </div>,
-  document.getElementById('app'),
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('app')
 );
