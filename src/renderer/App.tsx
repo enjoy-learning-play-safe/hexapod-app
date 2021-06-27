34838f4e907ccf7eed1593e015379c93a6bbe584
Dyllon <dyllon.g@outlook.com>
2021-06-27 12:16:53 +0800

    add chakra ui

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,16 +1,20 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import TitleBar from './components/TitleBar';
+import theme from './config/theme';
 import Router from './Router';
 
 function App() {
   return (
-    <BrowserRouter>
-      <TitleBar />
-      Hello I am react!
-      <Router />
-    </BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <TitleBar />
+        Hello I am react!
+        <Router />
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
